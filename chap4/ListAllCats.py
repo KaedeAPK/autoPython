55cdf636ee6449742082226f0e5eb4afb14008ce
Kaede <curiosity.yg57@gmail.com>
2018-12-21 19:15:44 +0900

    add for input list data

---
+++ chap4/ListAllCats.py
@@ -0,0 +1,11 @@
+catName1 = input('Type your cat01 name: ')
+catName2 = input('Type your cat02 name: ')
+catName3 = input('Type your cat03 name: ')
+catName4 = input('Type your cat04 name: ')
+catName5 = input('Type your cat05 name: ')
+catName6 = input('Type your cat06 name: ')
+
+print('Your Cats name are: \n'+
+catName1 + ' ' + catName2 + ' ' + catName3 + ' '+
+catName4 + ' ' + catName5 + ' ' + catName6 + ' '
+)
