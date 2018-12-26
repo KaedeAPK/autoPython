09ee96410cb927b9d1f7eab84b75646b63202173
Kaede <curiosity.yg57@gmail.com>
2018-12-27 02:16:58 +0900

    add blow hit sample

--- EasyPy/importRE.py
+++ EasyPy/importRE.py
@@ -11,6 +11,3 @@ print(b[0])
 print(b[1])
 print(b[2])
 print(b[3])
-
-
-
---
+++ chap4/ListCopy.py
@@ -0,0 +1,9 @@
+
+import copy
+spam = ['a','b','c','d',]
+cheese = copy.copy(spam)
+cheese[1] = 42
+print(spam, '\n', cheese)
+
+ani = ('Owl', 'Arigater', 'dog', 'cat')
+
