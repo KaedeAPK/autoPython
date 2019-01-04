03a162d94afc1074c841473ee100b6157452a689
Kaede <curiosity.yg57@gmail.com>
2019-01-05 02:48:05 +0900

    rm django

--- EasyPy/djangoGirls/reverse.py
+++
@@ -1,8 +0,0 @@
-
-lottery = [3, 42, 12, 19, 30, 59]
-
-# # sort(reverse = True)
-# lottery.reverse()
-# print(lottery)
-# lottery.sort()
-print(lottery[-1],lottery[-2], lottery[-3],lottery[-4],lottery[-6],)
